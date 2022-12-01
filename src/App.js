ec455b8db5ef07ecd8500540a9d47218e719516e
Aiden Light <aidenmaster87@gmail.com>
2022-12-01 12:53:45 -0600

    PreEntrega-1 V1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
+
+  let carrito = [];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar
+        numItems={carrito.length}
+        />
       </header>
+      <ItemListContainer
+      greeting={"Bienvenido a la Tienda en linea de Instrumentos Musicales"}
+      />
     </div>
   );
 }
